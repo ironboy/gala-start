001b999a1061b8b672bff7e70483aeb2002cbf88
Ironboy <ironboy@users.noreply.github.com>
2025-10-30 12:54:19 +0100

    single source truth for generating main menu and deciding what to do when you choose a menu item - now is main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 import start from './pages/start.js';
 import aboutUs from './pages/about-us.js';
+import products from './pages/products.js';
 
-// which function to run on which hash
+// Our menu: label to display in menu and 
+// function to run on menu choice
 const menu = {
-  "start": start,
-  "about-us": aboutUs
+  "start": { label: 'Start', function: start },
+  "about-us": { label: 'About us', function: aboutUs },
+  "products": { label: 'Our products', function: products }
 };
+
+function createMenu() {
+  // Object.entries -> convert object to array
+  // then map to create a-tags (links)
+  // then join everything into one big string
+  return Object.entries(menu)
+    .map(([urlHash, { label }]) => `
+      <a href="#${urlHash}">${label}</a>
+    `)
+    .join('');
+}
 
 function loadPageContent() {
   // if no hash redirect to #start
   if (location.hash === '') { location.replace('#start'); }
   // get the correct function to run depending on location.hash
-  const functionToRun = menu[location.hash.slice(1)];
+  const functionToRun = menu[location.hash.slice(1)].function;
   // run the function and expect it return a html string
   const html = functionToRun();
   // replace the contents of the main element
@@ -23,3 +37,6 @@ loadPageContent();
 
 // and then on every hash change of the url/location
 window.onhashchange = loadPageContent;
+
+// create the menu and display it
+document.querySelector('header nav').innerHTML = createMenu();
---
+++ js/pages/products.js
@@ -0,0 +1,6 @@
+export default function products() {
+  return `
+    <h1>Products</h1>
+    <p>Our products are beautiful</p>
+  `;
+}
